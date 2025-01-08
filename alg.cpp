10108544a13ebd629f0c5d119aa40f81f39af67d
UsrMakson <166782494+UsrMakson@users.noreply.github.com>
2025-01-08 18:23:21 +0500

    no works actualy

---
+++ alg.cpp
@@ -0,0 +1,13 @@
+
+#include "Header.h"
+#include <algorithm>
+#include <iostream>>
+#include <vector>
+
+using namespace std;
+
+void alg(int& n, int& res) {
+    for (int i = 0; i < n; i++) 
+        res *= 2;
+    
+}
---
+++ service_func.cpp
@@ -0,0 +1,12 @@
+
+#include "Header.h"
+#include <iostream>>
+#include <vector>
+
+using namespace std;
+
+void corrrect_cin(int& n) {
+    if ((n < 40) or (n > 2147483647)) {
+        do { cout << " : повторно введите N --> "; cin >> n; } while ((n < 40) or (n > 2147483647));
+    }
+}
