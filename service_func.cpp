56ae15cc7c2f6498fa18537c74f54e46d16002ec
UsrMakson <166782494+UsrMakson@users.noreply.github.com>
2025-01-09 18:54:55 +0500

    done correct checker

--- service_func.cpp
+++ service_func.cpp
@@ -1,12 +1,19 @@
 
 #include "Header.h"
-#include <iostream>>
-#include <vector>
+#include <iostream>
+#include <limits>
 
 using namespace std;
 
-void corrrect_cin(int& n) {
-    if ((n < 40) or (n > 2147483646)) {
-        do { cout << " : повторно введите N --> "; cin >> n; } while ((n < 40) or (n > 2147483647));
+void correct_cin(int& n) {
+    cout << " : ¬ведите N -> ";
+    cin >> n;
+
+    if (n < 40 || n >= 2147483646 || cin.fail()) {
+        do { 
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            cout << " : повторно введите N --> "; cin >> n; 
+        } while ((n < 40) or (n >= 2147483647));
     }
 }
